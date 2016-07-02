b6072832da2e7c8e996ab5756a5f7a31cb9623b5
Shih-Wen Su <sushi@summeridea.com>
2016-07-01 22:06:29 -0500

    add flipped card

---
+++ js/flipper.js
@@ -0,0 +1,14 @@
+(function() {
+  var cards = document.querySelectorAll(".flip-container");
+  for ( var i  = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    clickListener( card );
+  }
+
+  function clickListener(card) {
+    card.addEventListener( "click", function() {
+      var c = this.classList;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+    });
+  }
+})();
